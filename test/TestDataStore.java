0fb7ce609b3fe325779b74f750090027cf973e43
KarimEln <KarimEln123@gmail.com>
2024-09-30 19:16:11 -0400

    Test
    Updated test files

---
+++ test/TestDataStore.java
@@ -0,0 +1,23 @@
+package test;
+
+/** 
+ * Test-only implementation of DataStore that handles in-memory inputs and outputs
+ */
+public class TestDataStore implements DataStore {
+
+	@Override
+	public Iterable<Integer> read(InputConfig input) {
+		// Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
+		// another type of input. For production code, we'd want some better user input validation
+		return ((InMemoryInputConfig)input).getInputs();
+	}
+
+	@Override
+	public WriteResult appendSingleResult(OutputConfig output, String result) {
+		// Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
+		// another type of input. For production code, we'd want some better user input validation
+		((InMemoryOutputConfig)output).getOutputMutable().add(result);
+		return () -> WriteResult.WriteResultStatus.SUCCESS;
+	}
+
+}

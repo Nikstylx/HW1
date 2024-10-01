81c30b954db1d60950fcad805bf83b2009bcc413
KarimEln <KarimEln123@gmail.com>
2024-09-30 21:04:23 -0400

    fixes
    fixed bugs

--- src/ComputeEngineImpl.java
+++ src/ComputeEngineImpl.java
@@ -22,7 +22,9 @@ public class ComputeEngineImpl implements ComputeEngine {
     }
 
     private boolean isPrime(int num) {
-        if (num <= 1) return false;
+        if (num <= 1){
+            return false;
+        }
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;

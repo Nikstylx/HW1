fae3dbf12b9c1ed25b9f04bfcccd6c3d73034229
Nikstylx <201nick.h@gmail.com>
2024-09-30 20:37:48 -0400

    code added to compute

--- src/ComputeEngineImpl.java
+++ src/ComputeEngineImpl.java
@@ -6,5 +6,26 @@ public class ComputeEngineImpl implements ComputeEngine {
 	public String compute(int value) {
 		return "";
 	}
+	public static String findPrimesUpTo(int num) {
+	    StringBuilder primes = new StringBuilder();
+	    for (int i = 2; i <= num; i++) {
+	        if (isPrime(i)) {
+	            if (primes.length() > 0) {
+	                primes.append(", ");
+	            }
+	            primes.append(i);
+	        }
+	    }
+	    return primes.toString();
+	}
 
+	public static boolean isPrime(int num) {
+	    if (num <= 1) return false;
+	    for (int i = 2; i <= Math.sqrt(num); i++) {
+	        if (num % i == 0) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 }

d05ce7c08daa4ad0f14c47d66b655056e448fff9
Nikstylx <201nick.h@gmail.com>
2024-11-18 21:30:33 -0500

    Update Benchmarktest.java

--- src/Benchmark/Benchmarktest.java
+++ src/Benchmark/Benchmarktest.java
@@ -2,11 +2,13 @@ package edu.softwareeng.benchmark;
 
 import edu.softwareeng.sample.CoordinatorImplV1;
 import edu.softwareeng.sample.CoordinatorImplV2;
-import edu.softwareeng.sample.ComputeRequest;
-import edu.softwareeng.sample.InputConfig;
-import edu.softwareeng.sample.OutputConfig;
-import edu.softwareeng.sample.DataStore;
+import edu.softwareeng.sample.FileInputConfig;
+import edu.softwareeng.sample.FileOutputConfig;
 import edu.softwareeng.sample.ComputeEngine;
+import edu.softwareeng.sample.ComputeEngineImpl;
+import edu.softwareeng.sample.DataStore;
+import edu.softwareeng.sample.DataStoreImpl;
+import edu.softwareeng.sample.ComputeRequest;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Setup;
@@ -21,7 +23,7 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(org.openjdk.jmh.annotations.Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
-public class Benchmarktest implements InputConfig, OutputConfig, ComputeEngine, DataStore {
+public class CoordinatorBenchmark {
 
     private ComputeRequest requestV1;
     private ComputeRequest requestV2;
@@ -30,26 +32,33 @@ public class Benchmarktest implements InputConfig, OutputConfig, ComputeEngine, 
 
     @Setup(Level.Trial)
     public void setup() {
-       
+        // Create concrete instances of FileInputConfig and FileOutputConfig
+        FileInputConfig inputConfig = new FileInputConfig("test-data-source.txt");
+        FileOutputConfig outputConfig = new FileOutputConfig("test-output.txt");
 
         // Create ComputeRequest instances for both V1 and V2
         requestV1 = new ComputeRequest(inputConfig, outputConfig);
         requestV2 = new ComputeRequest(inputConfig, outputConfig);
 
-    
+        // Initialize ComputeEngine and DataStore
+        ComputeEngine computeEngine = new ComputeEngineImpl(); // Use ComputeEngineImpl
+        DataStore dataStore = new DataStoreImpl(); // Use DataStoreImpl for reading and writing data
 
-        // Initialize the coordinators
+        // Initialize the coordinators with the compute engine and data store
         coordinatorV1 = new CoordinatorImplV1(dataStore, computeEngine);
         coordinatorV2 = new CoordinatorImplV2(dataStore, computeEngine);
     }
 
     @Benchmark
     public void testCoordinatorV1() {
+        // Execute the computation with CoordinatorImplV1
         coordinatorV1.compute(requestV1);
     }
 
     @Benchmark
     public void testCoordinatorV2() {
+        // Execute the computation with CoordinatorImplV2
         coordinatorV2.compute(requestV2);
     }
 }
+

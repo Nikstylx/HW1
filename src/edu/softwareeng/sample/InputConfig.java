52a4e0b90d244f11666ac56221f927a575e92388
Nikstylx <201nick.h@gmail.com>
2024-12-12 01:27:40 -0500

    Update InputConfig.java

--- src/edu/softwareeng/sample/InputConfig.java
+++ src/edu/softwareeng/sample/InputConfig.java
@@ -1,16 +1,5 @@
 package edu.softwareeng.sample;
 
 public interface InputConfig {
-
-	// Use generics to allow callers to specify a return type when they visit the input data
-	static interface InputConfigVisitor<T> {
-		public T visitFile(FileInputConfig fileInputConfig);
-	}
-	
-	static <T> T visitInputConfig(InputConfig config, InputConfigVisitor<T> visitor) {
-		if (config instanceof FileInputConfig) {
-			return visitor.visitFile((FileInputConfig) config);
-		}
-		throw new RuntimeException("Unexpected config type: " + config.getClass());
-	}
+    String getFileName();
 }

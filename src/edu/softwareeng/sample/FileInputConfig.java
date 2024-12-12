fd5b32856aabb0e3f1df4413366f457433d5cf0e
Nikstylx <201nick.h@gmail.com>
2024-12-12 02:51:27 -0500

    Update FileInputConfig.java

--- src/edu/softwareeng/sample/FileInputConfig.java
+++ src/edu/softwareeng/sample/FileInputConfig.java
@@ -1,22 +1,25 @@
 package edu.softwareeng.sample;
 
-/**
- * Supports a text file input of numbers, one number per line.
- * 
- * Ex:
- * 1
- * 2
- * 10
- */
+import java.util.List;
+import java.util.ArrayList;
+
 public class FileInputConfig implements InputConfig {
+    private FileConfig fileConfig;  // Use FileConfig to hold the file path or related data
+    private List<Integer> inputs;
 
-	private final String fileName;
+    public FileInputConfig(FileConfig fileConfig, List<Integer> inputs) {
+        this.fileConfig = fileConfig;
+        this.inputs = inputs;
+    }
 
-	public FileInputConfig(String fileName) {
-		this.fileName = fileName;
-	}
+    public FileConfig getFileConfig() {
+        return fileConfig;
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
+    @Override
+    public List<Integer> getInputs() {
+        return inputs;
+    }
+
+    // You can add additional methods if needed to handle file operations
 }

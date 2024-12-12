018c683f3c5f7730bbe668e551c1bd91f09f0664
Nikstylx <201nick.h@gmail.com>
2024-12-12 00:59:26 -0500

    Update CoordinatorImpl.java

--- src/edu/softwareeng/sample/CoordinatorImpl.java
+++ src/edu/softwareeng/sample/CoordinatorImpl.java
@@ -1,6 +1,14 @@
+package edu.softwareeng.sample;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class CoordinatorImpl implements ComputationCoordinator {
 
@@ -73,4 +81,3 @@ public class CoordinatorImpl implements ComputationCoordinator {
         }
     }
 }
-

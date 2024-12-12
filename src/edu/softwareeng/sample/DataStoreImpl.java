df83eebcdd4265a35a9ae04ad4231dc8d40dfd3e
Nikstylx <201nick.h@gmail.com>
2024-12-12 02:53:46 -0500

    Update DataStoreImpl.java

--- src/edu/softwareeng/sample/DataStoreImpl.java
+++ src/edu/softwareeng/sample/DataStoreImpl.java
@@ -1,8 +1,15 @@
 package edu.softwareeng.sample;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class DataStoreImpl implements DataStore {
+
+    @Override
+    public void read(InputConfig inputConfig) {
+        // Example: Load data from the inputConfig, like reading from a file or fetching data
+        System.out.println("Reading data using the provided inputConfig.");
+    }
 
     @Override
     public WriteResult appendSingleResult(OutputConfig outputConfig, String result, char delimiter) {

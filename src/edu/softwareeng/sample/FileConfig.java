9dde9fcb5691d6945d26385ac4d2815f2c2ba884
Nikstylx <201nick.h@gmail.com>
2024-12-12 02:51:12 -0500

    Create FileConfig.java

---
+++ src/edu/softwareeng/sample/FileConfig.java
@@ -0,0 +1,13 @@
+package edu.softwareeng.sample;
+
+public class FileConfig {
+    private String filePath;
+
+    public FileConfig(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+}

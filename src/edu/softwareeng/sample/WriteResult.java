db15819f34a527f89f7beb10513ffbead7e7ad2c
Nikstylx <201nick.h@gmail.com>
2024-12-12 02:54:51 -0500

    Update WriteResult.java

--- src/edu/softwareeng/sample/WriteResult.java
+++ src/edu/softwareeng/sample/WriteResult.java
@@ -1,9 +1,5 @@
 package edu.softwareeng.sample;
-public interface WriteResult {
-	WriteResultStatus getStatus();
-	
-	public static enum WriteResultStatus {
-		SUCCESS,
-		FAILURE;
-	}
+
+public enum WriteResult {
+    SUCCESS, FAILURE
 }

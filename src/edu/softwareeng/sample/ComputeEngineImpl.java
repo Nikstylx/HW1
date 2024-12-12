811d8b7b27585a446702bf096f3292b28b90b0ac
Nikstylx <201nick.h@gmail.com>
2024-12-12 02:34:17 -0500

    Update ComputeEngineImpl.java

--- src/edu/softwareeng/sample/ComputeEngineImpl.java
+++ src/edu/softwareeng/sample/ComputeEngineImpl.java
@@ -1,37 +1,44 @@
-package edu.softwareeng.sample;
-
-import java.util.List;
-
 public class ComputeEngineImpl implements ComputeEngine {
 
     @Override
-    public void compute(InputConfig input, OutputConfig output, char delimiter) {
-        // Get the list of inputs
-        List<Integer> inputs = ((InMemoryInputConfig) input).getInputs();
-        
-        // Get the mutable list of outputs
-        List<String> outputList = ((InMemoryOutputConfig) output).getOutputMutable();
-        
-        // Iterate over each input, check if it's prime, and append result to output list
-        for (Integer num : inputs) {
-            try {
-                if (num < 0) {
-                    throw new IllegalArgumentException("Input cannot be negative: " + num);
-                }
-                if (isPrime(num)) {
-                    outputList.add(num + " is prime");
-                } else {
-                    outputList.add(num + " is not prime");
-                }
-            } catch (IllegalArgumentException e) {
-                outputList.add("Error: " + e.getMessage());
-            } catch (Exception e) {
-                outputList.add("Unexpected error with input " + num + ": " + e.getMessage());
-            }
+    public String compute(int num) {
+        // Implement the logic to handle a single integer, for example, check if it's prime
+        if (num < 0) {
+            throw new IllegalArgumentException("Input number cannot be negative");
+        }
+
+        if (isPrime(num)) {
+            return num + " is prime";
+        } else {
+            return num + " is not prime";
         }
     }
 
-    // Helper method to check if a number is prime
+    @Override
+    public String compute(InputConfig inputConfig, OutputConfig outputConfig, char delimiter) {
+        // Existing logic for processing inputs and outputs using the provided InputConfig and OutputConfig
+        List<Integer> inputs = inputConfig.getInputs();
+        List<String> results = new ArrayList<>();
+        
+        // Process each number and check if it's prime
+        for (Integer num : inputs) {
+            if (num < 0) {
+                throw new IllegalArgumentException("Input number cannot be negative");
+            }
+            
+            if (isPrime(num)) {
+                results.add(num + " is prime");
+            } else {
+                results.add(num + " is not prime");
+            }
+        }
+
+        // Write the results to the outputConfig
+        outputConfig.appendResults(results, delimiter);
+
+        return "Computation complete.";
+    }
+
     private boolean isPrime(int num) {
         if (num <= 1) {
             return false; // 0 and 1 are not prime numbers

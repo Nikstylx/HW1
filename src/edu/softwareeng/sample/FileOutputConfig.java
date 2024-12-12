1ca8389de7664ffa1dfcfe7e57d07951609ed354
Nikstylx <201nick.h@gmail.com>
2024-12-12 03:02:54 -0500

    Update FileOutputConfig.java

--- src/edu/softwareeng/sample/FileOutputConfig.java
+++ src/edu/softwareeng/sample/FileOutputConfig.java
@@ -15,4 +15,10 @@ public class FileOutputConfig implements OutputConfig {
 	public String getFileName() {
 		return fileName;
 	}
+	@Override
+public void appendResults(List<String> results, char delimiter) {
+    // Implement the method to handle appending results
+    // Example: write the results to a file or append to an internal list
+}
+
 }
